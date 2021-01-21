bac395500a87cfff1fc56da06492e678011da010
conyeda <saulbv.tic@outlook.com>
2021-01-21 18:41:46 +0100

    Solved exercise UOJ 10954: add all

--- uva-online-judge/10954-add-all/main.cpp
+++ uva-online-judge/10954-add-all/main.cpp
@@ -7,9 +7,42 @@ typedef vector<ii> 			vii;
 typedef vector<int> 		vi;
 #define INF 1000000000;
 
+int N;
+
+class comparator
+{
+  public:
+  bool operator() (int a, int b) {
+    return -a < -b;
+  }
+};
+
 void solve()
 {
-
+	priority_queue<int, vector<int>, comparator> numbers;
+	int num = 0;
+	int min1 = 0, min2 = 0;
+	int sum = 0, cost = 0;
+	
+	while (N--)
+	{
+		cin >> num;
+		numbers.push(num);
+	}
+	
+	while (numbers.size() > 1)
+	{
+		min1 = numbers.top();
+		numbers.pop();
+		min2 = numbers.top();
+		numbers.pop();
+		
+		sum = min1 + min2;
+		cost += sum;
+		numbers.push(sum);
+	}
+	
+	cout << cost;
 }
 
 int main()
@@ -22,14 +55,15 @@ int main()
     freopen("my-output.txt", "w", stdout);
     freopen("error.txt", "w", stderr);
     #endif
+    
+    cin >> N;
 
-    int t = 1;
-    cin >> t;
-
-    while (t--)
+    while (N != 0)
     {
         solve();
         cout << "\n";
+        
+        cin >> N;
     }
 
     cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
