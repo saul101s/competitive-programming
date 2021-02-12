f5a311412c45b7b98c50ca70c98f15f16dac90a2
Saúl Blanco <saulbv.tic@outlook.com>
2021-02-12 12:43:59 +0100

    Solved exercise 10055: hashmat the brave warrior

---
+++ uva-online-judge/10055-hashmat-the-brave-warrior/main.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+long a, b;
+
+void solve()
+{
+  cout << abs(b - a);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  while (cin >> a >> b)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
