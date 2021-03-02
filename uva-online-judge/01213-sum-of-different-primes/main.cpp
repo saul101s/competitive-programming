5d8b63fee80711f5cd588275a4b5ddc70d7e294b
Saúl Blanco <saulbv.tic@outlook.com>
2021-03-02 16:57:51 +0100

    Solved exercise UOJ 01213: sum of different primes

---
+++ uva-online-judge/01213-sum-of-different-primes/main.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+int n, k;
+constexpr int NUM_PRIMES = 187;
+int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117};
+long memo[NUM_PRIMES][1121][15];
+
+long bag(int prime, int remN, int remK)
+{
+  if (remN < 0) return 0;
+  if (remK == 0) return (remN == 0 ? 1 : 0);
+  if (prime == NUM_PRIMES || primes[prime] > remN) return 0;
+  if (memo[prime][remN][remK] != -1) return memo[prime][remN][remK];
+  
+  return memo[prime][remN][remK] = bag(prime + 1, remN-primes[prime], remK-1) +
+                                   bag(prime + 1, remN, remK);
+}
+
+void solve()
+{
+  cout << bag(0, n, k);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  memset(memo, -1, sizeof memo);
+  while (cin >> n >> k && n && k)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
