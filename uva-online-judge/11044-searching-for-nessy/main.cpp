36b92ad11c22ad7a0a38ab0ffdd2a25ad87905e6
Saúl Blanco <saulbv.tic@outlook.com>
2021-03-08 22:54:10 +0100

    Solved exercise UOJ 11044: searching for nessy

---
+++ uva-online-judge/11044-searching-for-nessy/main.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+void solve()
+{
+  int n, m, n_beams, m_beams;
+  
+  cin >> n >> m;
+  n -= 2;
+  m -= 2;
+  
+  n_beams = ceil(n / 3.0);
+  m_beams = ceil(m / 3.0);
+  
+  cout << n_beams * m_beams;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
