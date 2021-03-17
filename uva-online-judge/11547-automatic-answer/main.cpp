d590beadf2188b1d07773b54007b9d7c11d9409e
Saúl Blanco <saulbv.tic@outlook.com>
2021-03-17 22:43:20 +0100

    Solved exercise UOJ 11547: automatic answer

---
+++ uva-online-judge/11547-automatic-answer/main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+void solve()
+{
+  int n;
+  cin >> n;
+  cout << abs(((315 * n + 36962) / 10) % 10);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
