c51202017ad5655e8c6c4ebb02e11efe31bcc27a
Saúl Blanco <saulbv.tic@outlook.com>
2021-03-23 19:07:37 +0100

    Solved exercise UOJ 12372: packing for holiday

---
+++ uva-online-judge/12372-packing-for-holiday/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+int c = 1;
+int L, W, H;
+
+void solve()
+{
+  cin >> L >> W >> H;
+  
+  cout << "Case " << c++ << ": ";
+  
+  if (L <= 20 && W <= 20 && H <= 20) cout << "good";
+  else cout << "bad";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
