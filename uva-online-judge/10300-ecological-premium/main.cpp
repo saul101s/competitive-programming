1d3864ecadd5cf0faead5b4be91ba484be5202af
Saúl Blanco <saul101.pi@outlook.com>
2022-01-31 16:59:22 +0100

    Solved exercise UOJ 10300: ecological premium

---
+++ uva-online-judge/10300-ecological-premium/main.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+void solve()
+{
+  int f;
+  cin >> f;
+  
+  unsigned long long premium = 0;
+  
+  while (f--)
+  {
+    long sz, animals, envf;
+    cin >> sz >> animals >> envf;
+    
+    premium += sz * envf;
+  }
+  
+  cout << premium;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
