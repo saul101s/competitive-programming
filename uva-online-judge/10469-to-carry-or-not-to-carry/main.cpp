8011d3e4d729755eefa236e1ee4098e677830b13
saul101s <saul101.pi@outlook.com>
2025-06-05 18:56:14 +0200

    Solve exercise UOJ 10469: to carry or not to carry

---
+++ uva-online-judge/10469-to-carry-or-not-to-carry/main.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long         ll;
+typedef pair<int, int>    ii;
+typedef vector<ii>        vii;
+typedef vector<int>       vi;
+#define INF 1000000000
+
+unsigned int a = 0;
+unsigned int b = 0;
+
+void solve()
+{
+  unsigned int res = a ^ b;
+  cout << res;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("my-output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+  #endif
+  
+  while (cin >> a && cin >> b)
+  {
+    solve();
+    cout << "\n";
+  }
+
+  cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+  return 0;
+}
